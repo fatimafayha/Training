ab2562aed14332fa9a0cd390cde8760f2b2bbc82
Fatima Fayha <f.begumfayha@reply.com>
2025-02-19 15:34:49 -0500

    Derived state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import "./App.css";
 import Welcome from "./Concepts/props";
-import NameInput from "./Concepts/stateLifting";
 
 function App() {
-  const [name, setName] = useState(" "); 
+  const [inputValue, setInputValue] = useState("");
+  const name = inputValue.trim() ? inputValue : "";
 
   return (
-    <>
-      <Welcome name={name} />
-      <NameInput onNameChange={setName} />
-    </>
+    <div className="container">
+      <div className={`welcome-container ${name ? "" : "hidden"}`}>
+        <Welcome name={name} />
+      </div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter your name"
+      />
+    </div>
   );
 }
 
